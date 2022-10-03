221d9a3a850616251ab4e01abf960be7124855b6
Raul <alejandro.june20@gmail.com>
2022-10-02 23:46:02 -0400

    server and client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-const express = require('express')
-const app= express()
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-app.get("/api", (req,res)=> {
-    res.json({"users": ["u1","u2","u3"]})
-})
-app.listen(5000,()=>{
-    console.log("server started on port 5000")
-})
+import postRoutes from './routes/posts.js';
+
+const app= express();
+
+app.use(bodyParser.json()) 
+
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(cors());
+app.use('/posts',postRoutes)
+
+//mongo cloud atlas
+const CONNECTION_URL = '';
+const PORT = process.env.PORT || 5000; 
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology: true})
+.then(()=> app.listen(PORT,() => console.log(`Server running on port ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
+
